520d865b4b2822c66f4cc877f06f8ba3a5749d40
v3launchunit <lengland0317@gmail.com>
2024-10-05 15:07:25 -0500

    started work on level transition system

---
+++ trench_broom/func_godot_fgd/entities/solid/level_exit.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+
+
+@export var func_godot_properties: Dictionary
+
+@onready var level := get_tree().current_scene as Level
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func get_tooltip() -> String:
+	return Globals.parse_text("tooltips", func_godot_properties["tooltip"])
+
+
+func interact(with: Node3D) -> void:
+	var time := level.time
+	var foes := level.foes
+	var kills := level.kills
+	var secrets := level.secrets
+	var found_secrets := level.found_secrets
+	
+	Intermission.run_intermission(with, time, foes, kills, secrets, found_secrets, func_godot_properties["next"])
