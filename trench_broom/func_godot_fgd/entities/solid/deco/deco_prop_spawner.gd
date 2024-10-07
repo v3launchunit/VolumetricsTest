cad5c276d92131e936e8039a60ba000a0e58160b
v3launchunit <lengland0317@gmail.com>
2024-10-07 07:11:49 -0500

    reorganized files

---
+++ trench_broom/func_godot_fgd/entities/solid/deco/deco_prop_spawner.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+
+@export var func_godot_properties: Dictionary:
+	set(to):
+		func_godot_properties = to
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
