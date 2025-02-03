61cbe199fb213a08d540c2ed05ab391569961de8
v3launchunit <lengland0317@gmail.com>
2025-02-03 09:21:25 -0600

    idk

---
+++ ui/menus/settings/accessibility_sub_menu.gd
@@ -0,0 +1,15 @@
+extends VBoxContainer
+
+
+@onready var _lang_dropdown := find_child("LanguageDropdown") as OptionButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#_lang_dropdown
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
