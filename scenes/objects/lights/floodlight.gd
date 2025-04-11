41ebcab437678c78783d080715bdae0fc71f352f
v3launchunit <lengland0317@gmail.com>
2025-04-11 09:37:20 -0500

    more materials work (better planks etc) made trash table

--- scenes/objects/lights/floodlight.gd
+++ scenes/objects/lights/floodlight.gd
@@ -14,33 +14,31 @@ extends RigidBody3D
 		if func_godot_properties.get("killtarget", "") != "":
 			add_to_group("targets:%s" %  func_godot_properties["killtarget"], true)
 		
-		await (ready)
-		await (get_tree().process_frame)
-		
-		if func_godot_properties.get("target", "") != "":
-			($floodlight/Stand/Neck_L/Lamp_L as Node3D).look_at(
-					(
-							get_tree().get_first_node_in_group(
-									"targetname:%s" % func_godot_properties["target"]
-							) as Node3D
-					).global_position,
-					Vector3.UP,
-					true
-			)
-		if func_godot_properties.get("killtarget", "") != "":
-			($floodlight/Stand/Neck_R/Lamp_R as Node3D).look_at(
-					(
-							get_tree().get_first_node_in_group(
-									"targetname:%s" % func_godot_properties["killtarget"]
-							) as Node3D
-					).global_position,
-					Vector3.UP,
-					true
-			)
+		#await (ready)
+		#await (get_tree().process_frame)
 
 
 func _func_godot_build_complete() -> void:
-	pass
+	if func_godot_properties.get("target", "") != "":
+		($floodlight/Stand/Neck_L/Lamp_L as Node3D).look_at(
+				(
+						get_tree().get_first_node_in_group(
+								"targetname:%s" % func_godot_properties["target"]
+						) as Node3D
+				).global_position,
+				Vector3.UP,
+				true
+		)
+	if func_godot_properties.get("killtarget", "") != "":
+		($floodlight/Stand/Neck_R/Lamp_R as Node3D).look_at(
+				(
+						get_tree().get_first_node_in_group(
+								"targetname:%s" % func_godot_properties["killtarget"]
+						) as Node3D
+				).global_position,
+				Vector3.UP,
+				true
+		)
 
 
 func _ready() -> void:
