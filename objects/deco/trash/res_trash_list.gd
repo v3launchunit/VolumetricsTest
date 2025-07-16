c83128733a452b5be942f7de8c4bca6942fe4207
v3launchunit <lengland0317@gmail.com>
2025-07-16 10:35:45 -0500

    prepared to migrate trash prop list to resource

---
+++ objects/deco/trash/res_trash_list.gd
@@ -0,0 +1,22 @@
+class_name TrashList
+extends Resource
+
+@export var base_lists: Array[TrashList]
+@export var props: Array[TrashProp]
+
+func get_prop() -> PackedScene:
+	return get_pool().pick_random()
+
+func get_pool() -> Array[PackedScene]:
+	var pool : Array[PackedScene]
+	
+	for list: TrashList in base_lists:
+		pool.append_array(list.get_pool())
+	
+	for prop: TrashProp in props:
+		for i in range(floori(prop.weight)):
+			pool.append(prop.scene)
+		if randi() > prop.weight - floorf(prop.weight):
+			pool.append(prop.scene)
+	
+	return pool
---
+++ objects/deco/trash/res_trash_prop.gd
@@ -0,0 +1,5 @@
+class_name TrashProp 
+extends Resource
+
+@export var scene: PackedScene
+@export var weight: float = 1.0
